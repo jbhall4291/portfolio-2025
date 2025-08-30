eb01a00f11ce9a299fa1fcbf8912fe371b3818bc
Johnny Hall <jbhall4291@gmail.com>
2025-08-31 00:10:32 +0100

    add correct email for mailto

--- components/site/footer.tsx
+++ components/site/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="mt-20 mb-6 text-center text-xs">
             <a
-                href={`mailto:johnny@example.com?subject=Hello%20Johnny%20—%20Re:%20Portfolio&body=Hi%20Johnny,%0D%0A%0D%0AQuick%20intro...`}
+                href={`mailto:jbhall4291@gmail.com?subject=Hello%20Johnny!`}
                 className=""
             >© {new Date().getFullYear()} Johnny Hall</a>
         </footer>

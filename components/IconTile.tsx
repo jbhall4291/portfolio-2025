d9812329a1e6baa55b31503a1f5092b71a2aed95
Johnny Hall <jbhall4291@gmail.com>
2025-09-15 20:56:03 +0100

    refactor rendering logic

--- components/IconTile.tsx
+++ components/IconTile.tsx
@@ -5,26 +5,41 @@ type Props = {
     bg: string;   // brand/background colour
     pad?: number; // mobile padding in px (default 2)
     padLg?: number; // desktop padding in px (default = pad)
+    hasBorder?: boolean;
 };
 
-export default function IconTile({ src, alt = "", bg, pad = 2, padLg }: Props) {
+export default function IconTile({ src, alt = "", bg, pad = 2, padLg, hasBorder }: Props) {
     return (
+        // <div
+        //     aria-hidden
+        //     className="relative overflow-hidden pointer-events-none will-change-transform
+        //          w-[56px] h-[56px] lg:w-[80px] lg:h-[80px]
+        //          rounded-[8px] md:rounded-[10px]
+        //          "
+        //     style={{ background: bg }}
+        // >
+        //     <div
+        //         className="absolute inset-0 p-[var(--pad)] lg:p-[var(--pad-lg)]"
+        //         style={
+        //             { ["--pad" as any]: `${pad}px`, ["--pad-lg" as any]: `${padLg ?? pad}px` }
+        //         }
+        //     >
+        //         <img src={src} alt={alt} className="block w-full h-full" />
+        //     </div>
+        // </div>
+
         <div
-            aria-hidden
-            className="relative overflow-hidden pointer-events-none will-change-transform
-                 w-[56px] h-[56px] lg:w-[80px] lg:h-[80px]
-                 rounded-[8px] md:rounded-[10px]
-                 "
-            style={{ background: bg }}
+            className={` rounded-xl   w-[56px] h-[56px] lg:w-[80px] lg:h-[80px] hover:opacity-90 transition-all duration-500
+                
+                 ${hasBorder ? "border-2   border-foreground/5 dark:border-white" : ""}
+                `}
+            style={{ background: bg, padding: pad }}
         >
-            <div
-                className="absolute inset-0 p-[var(--pad)] lg:p-[var(--pad-lg)]"
-                style={
-                    { ["--pad" as any]: `${pad}px`, ["--pad-lg" as any]: `${padLg ?? pad}px` }
-                }
-            >
-                <img src={src} alt={alt} className="block w-full h-full" />
-            </div>
+
+            <img src={src} alt={alt} className="block w-full h-full" />
         </div>
+
+
+
     );
 }

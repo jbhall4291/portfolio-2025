bd2e81bcb4ae3b0d63038435f7a03758d9a22230
Johnny Hall <jbhall4291@gmail.com>
2025-09-26 21:28:33 +0100

    tweak icons

--- components/IconTile.tsx
+++ components/IconTile.tsx
@@ -27,7 +27,7 @@ export default function IconTile({
 }: Props) {
     const baseClass =
         `pointer-events-auto rounded-xl w-[52px] h-[52px] lg:w-[80px] lg:h-[80px] ` +
-        `transition-colors will-change-transform transform-gpu duration-300 p-[var(--pad)] lg:p-[var(--pad-lg)] ` +
+        `transition-colors will-change-transform transform-gpu duration-300 p-[var(--pad)] lg:p-[var(--pad-lg)] origin-center ` +
         (hasBorder ? "border-2 border-foreground/5 dark:border-white" : "");
 
     const style: React.CSSProperties = {
@@ -53,7 +53,13 @@ export default function IconTile({
                 className={baseClass}
                 style={style}
             >
-                <img src={src} alt={alt} decoding="async" className="block w-full h-full" />
+                <img
+                    src={src}
+                    alt={alt}
+                    draggable="false"
+                    decoding="async"
+                    loading="eager"
+                    className="block w-full h-full" />
             </motion.div>
         );
     }

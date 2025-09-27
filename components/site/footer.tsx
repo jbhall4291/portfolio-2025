23de8a8bc6a8a34e06eaad960cae7922d61993d4
Johnny Hall <jbhall4291@gmail.com>
2025-09-27 14:50:01 +0100

    remove email link

--- components/site/footer.tsx
+++ components/site/footer.tsx
@@ -2,10 +2,7 @@
 export default function Footer() {
     return (
         <footer className="mt-20 mb-6 text-center text-xs">
-            <a
-                href={`mailto:hello@johnnyhall.dev?subject=Hello%20Johnny!`}
-                className=""
-            >© {new Date().getFullYear()} Johnny Hall</a>
+            © {new Date().getFullYear()} Johnny Hall
         </footer>
     );
 }
